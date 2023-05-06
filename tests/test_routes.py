366f1bb0d7b02ee2e618ff6a317fcc13ae4ab788
davidjdeg2 <123874938+davidjdeg2@users.noreply.github.com>
2023-05-05 23:37:24 -0400

    Update test_routes.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,1 +1,21 @@
+import json
+from .models import db, Debt
+from .main import app
 
+def test_get_debt():
+    with app.test_client() as client:
+        # Create a new debt
+        debt = Debt(debtor='Knox Seabolt', amount_owed=.40, interest_rate=4)
+        db.session.add(debt)
+        db.session.commit()
+
+        # Send a GET request to /debt/1
+        response = client.get('/debt/1')
+        print(response)
+
+        # Check that the response is successful and has the expected data
+        assert response.status_code == 200
+        data = json.loads(response.data)
+        assert data['debtor'] == 'Knox Seabolt'
+        assert data['amount_owed'] == .40
+        assert data['interest_rate'] == 4
