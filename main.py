5f3dc997e487416190e3f2e317778d4905181f39
davidjdeg2 <123874938+davidjdeg2@users.noreply.github.com>
2023-05-05 23:26:17 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+from flask import Flask
+from .models import db
+from .routes import *
+from .auth import *
 
+app = Flask(__name__)
+app.config.from_object('config')
+
+db.init_app(app)
+
+if __name__ == '__main__':
+    app.run()
