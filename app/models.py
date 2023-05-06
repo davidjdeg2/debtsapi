8221ac79093e0b1c642045d8528e01aa1781237a
davidjdeg2 <123874938+davidjdeg2@users.noreply.github.com>
2023-05-05 20:07:14 -0400

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,33 +1,25 @@
-from flask import jsonify, request
-from .models import Debt, db
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
 
-@app.route('/debts', methods=['GET'])
-def get_debts():
-    debts = Debt.query.all()
-    return jsonify([debt.__dict__ for debt in debts])
+db = SQLAlchemy()
 
-@app.route('/debts/<int:debt_id>/add', methods=['PUT'])
-def add_to_debt(debt_id):
-    debt = Debt.query.get_or_404(debt_id)
-    data = request.get_json()
-    amount = data['amount']
-    debt.add_to_debt(amount)
-    db.session.commit()
-    return jsonify(debt.__dict__)
+class Debt(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    debtor = db.Column(db.String(255), nullable=False)
+    amount_owed = db.Column(db.Float, nullable=False)
+    interest_rate = db.Column(db.Float, nullable=False, default=0.02)
+    date_incurred = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
-@app.route('/debts/<int:debt_id>/subtract', methods=['PUT'])
-def subtract_from_debt(debt_id):
-    debt = Debt.query.get_or_404(debt_id)
-    data = request.get_json()
-    amount = data['amount']
-    debt.subtract_from_debt(amount)
-    db.session.commit()
-    return jsonify(debt.__dict__)
+    def __repr__(self):
+        return f'<Debt {self.id}>'
 
-@app.route('/debts/<int:debt_id>/interest', methods=['GET'])
-def calculate_interest(debt_id):
-    debt = Debt.query.get_or_404(debt_id)
-    data = request.args
-    months = int(data.get('months', 1))
-    interest = debt.calculate_interest(months)
-    return jsonify
+    def add_to_debt(self, amount):
+        self.amount_owed += amount
+        self.date_incurred = datetime.utcnow()
+
+    def subtract_from_debt(self, amount):
+        self.amount_owed -= amount
+        self.date_incurred = datetime.utcnow()
+
+    def calculate_interest(self, months):
+        return self.amount_owed * (1 + self.interest_rate) ** months - self.amount_owed
