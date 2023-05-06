48270d4c37db055625d69458d4448f14a4a95739
davidjdeg2 <123874938+davidjdeg2@users.noreply.github.com>
2023-05-05 20:15:08 -0400

    Create auth.py

---
+++ app/auth.py
@@ -0,0 +1,15 @@
+from flask import jsonify, request
+from werkzeug.security import check_password_hash
+from .models import User
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    username = data['username']
+    password = data['password']
+    user = User.query.filter_by(username=username).first()
+    if not user or not check_password_hash(user.password, password):
+        return jsonify({'error': 'Invalid username or password'}), 401
+    # At this point, the user has been authenticated successfully
+    # You might generate and return a JWT token here for subsequent authenticated requests
+    return jsonify({'message': 'Login successful'}), 200
