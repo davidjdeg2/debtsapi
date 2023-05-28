75344039a96793660dfbff0278d8e0353eace65e
davidjdeg2 <123874938+davidjdeg2@users.noreply.github.com>
2023-05-28 17:47:23 -0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,17 +1,13 @@
 from flask import Flask
-from models import db
-from auth import auth
-from routes import api_bp
+from .models import db
+from .auth import create_auth_blueprint
 
 app = Flask(__name__)
-app.config.from_object('config')
-
-# Register the API blueprint
-app.register_blueprint(api_bp)
 
 db.init_app(app)
-app.register_blueprint(api_bp, url_prefix='/api')
-auth.init_app(app)
+
+auth_bp = create_auth_blueprint(app)
+app.register_blueprint(auth_bp)
 
 if __name__ == '__main__':
-        app.run()
+    app.run()
