5bcd0ae929be0c3b111f28843f01d190a2d957fd
davidjdeg2 <123874938+davidjdeg2@users.noreply.github.com>
2023-05-28 17:25:16 -0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,9 +1,13 @@
 from flask import Flask
 from models import db
 from auth import auth
+from routes import api_bp
 
 app = Flask(__name__)
 app.config.from_object('config')
+
+# Register the API blueprint
+app.register_blueprint(api_bp)
 
 db.init_app(app)
 app.register_blueprint(api_bp, url_prefix='/api')
