de50dd0013edd6ac4960f1494ead504a7ed727a8
davidjdeg2 <123874938+davidjdeg2@users.noreply.github.com>
2023-05-28 17:20:23 -0400

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,9 @@
-from flask import jsonify, request
+from flask import Blueprint, jsonify, request
 from models import Debt, db
-from main import app
 
-@app.route('/debt/<int:id>', methods=['GET'])
+api_bp = Blueprint('api', __name__)
+
+@api_bp.route('/debt/<int:id>', methods=['GET'])
 def get_debt(id):
     debt = Debt.query.get(id)
     if not debt:
@@ -16,7 +17,7 @@ def get_debt(id):
         'total_owed': total_owed
     }), 200
 
-@app.route('/debts/<int:debt_id>/add', methods=['PUT'])
+@api_bp.route('/debts/<int:debt_id>/add', methods=['PUT'])
 def add_to_debt(debt_id):
     debt = Debt.query.get_or_404(debt_id)
     data = request.get_json()
@@ -25,7 +26,7 @@ def add_to_debt(debt_id):
     db.session.commit()
     return jsonify(debt.__dict__)
 
-@app.route('/debts/<int:debt_id>/subtract', methods=['PUT'])
+@api_bp.route('/debts/<int:debt_id>/subtract', methods=['PUT'])
 def subtract_from_debt(debt_id):
     debt = Debt.query.get_or_404(debt_id)
     data = request.get_json()
@@ -34,7 +35,7 @@ def subtract_from_debt(debt_id):
     db.session.commit()
     return jsonify(debt.__dict__)
 
-@app.route('/debts/<int:debt_id>/interest', methods=['GET'])
+@api_bp.route('/debts/<int:debt_id>/interest', methods=['GET'])
 def calculate_interest(debt_id):
     debt = Debt.query.get_or_404(debt_id)
     data = request.args
