38d736186ee601340d1d690402bb2e4a42288948
David Degaraff <123874938+davidjdeg2@users.noreply.github.com>
2023-05-30 08:16:45 -0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,13 +2,7 @@ from flask import Flask
 from models import db
 from auth import create_auth_blueprint
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db = SQLAlchemy(app)
-
-db.init_app(app)
+create_app(app)
 
 auth_bp = create_auth_blueprint(app)
 app.register_blueprint(auth_bp)
